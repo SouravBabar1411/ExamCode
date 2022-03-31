1e602db4e0f50eed5f6576f54d23af7229a8f247
sourav1411 <babarsourav12@gmail.com>
2022-03-31 12:54:53 +0530

    added roles for through enum admin and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #Enum for assigning roles
+  enum role: [:admin, :user]
 end
